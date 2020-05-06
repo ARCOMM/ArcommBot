86920bd3eb1411aecc16d4f4d3d493cc6fdc4d0e
TomBurch <thomas.burch1306@gmail.com>
2020-05-06 22:34:22 +0100

    Added getRoles, searchRoles, roleListKey to utility

--- cogs/utility.py
+++ cogs/utility.py
@@ -18,6 +18,48 @@ class Utility(commands.Cog):
 
         return newMessage
 
+    def getRoles(self, ctx, reserved = False, sort = False):
+        logger.debug("getRoles called")
+        
+        roles = ctx.message.author.guild.roles[1:]
+
+        if sort:
+            roles.sort(key = self.roleListKey)
+
+        if not reserved:
+            newRoles = []
+            for role in roles:
+                if role.colour.value == 0:
+                    newRoles.append(role)
+            return newRoles
+        else:
+            return roles
+    
+    def searchRoles(self, ctx, roleQuery, autocomplete = False, reserved = False):
+        logger.debug("searchRoles called")
+
+        roles = self.getRoles(ctx, reserved = reserved)
+        roleQuery = roleQuery.lower()
+        candidate = None
+
+        for role in roles:
+            roleName = role.name.lower()
+            if roleName == roleQuery:
+                candidate = role
+                break
+            elif autocomplete and re.match(re.escape(roleQuery), roleName):
+                candidate = role
+
+        if candidate:
+            if candidate.colour.value == 0:
+                return candidate
+            else:
+                return "RESERVED"
+        else:
+            return None
+
+    def roleListKey(self, elem):
+        return elem.name.lower()
 
 def setup(bot):
     bot.add_cog(Utility(bot))
