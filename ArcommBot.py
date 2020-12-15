f5105f8a492fd4e34227d289b8c62b3020a59acf
TomBurch <thomas.burch1306@gmail.com>
2020-12-15 22:49:29 +0000

    Flake8

--- ArcommBot.py
+++ ArcommBot.py
@@ -19,31 +19,36 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix = '.', case_insensitive = True, intents = intents)
 
+
 @bot.event
 async def on_message(message):
     try:
         await bot.process_commands(message)
-    except Exception as e:
+    except Exception:
         exc = sys.exc_info()
         await message.channel.send("Type [{}], Value [{}]\nTraceback[{}]".format(exc[0], exc[1], exc[2]))
+
 
 def setupLogging():
     logger = logging.getLogger('discord')
     logger.setLevel(logging.DEBUG)
-    handler = logging.handlers.TimedRotatingFileHandler(filename = 'logs/discord.log', when = 'h', interval = 8, backupCount = 3, encoding = 'utf-8')
+    handler = logging.handlers.TimedRotatingFileHandler(filename = 'logs/discord.log', when = 'h', interval = 8,
+                                                        backupCount = 3, encoding = 'utf-8')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
 
     logger = logging.getLogger('bot')
     logger.setLevel(logging.DEBUG)
-    handler = logging.handlers.TimedRotatingFileHandler(filename = 'logs/bot.log', when = 'h', interval = 8, backupCount = 3, encoding = 'utf-8')
+    handler = logging.handlers.TimedRotatingFileHandler(filename = 'logs/bot.log', when = 'h', interval = 8,
+                                                        backupCount = 3, encoding = 'utf-8')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
+
 
 def loadExtensions():
     logger = logging.getLogger('bot')
     startupExtensions = config['cogs']
-    
+
     for extension in startupExtensions:
         try:
             bot.load_extension("cogs." + extension)
@@ -53,15 +58,17 @@ def loadExtensions():
             logger.critical("Failed to load {} extension\n".format(extension))
             logger.critical(e)   
 
+
 def restart():
     subprocess.call(["python", os.path.join(sys.path[0], __file__)] + sys.argv[1:])
+
 
 if __name__ == "__main__":
     setupLogging()
     loadExtensions()
 
-    while True:     
+    while True:
         bot.loop.run_until_complete(bot.start(TOKEN))
-        
+
         print("Reconnecting")
         bot.client = commands.Bot(command_prefix = '.', case_insensitive = True, intents = intents, loop = bot.loop)
